ba22c260f0a8837c121dd942e3e9e15dfdae043b
Pat Lawler <patlaw777@icloud.com>
2022-03-06 13:09:42 -0600

    connected the server and sql

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const mysql = require('mysql2');
+
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,12 +8,27 @@ const app = express();
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
 
+const db = mysql.createConnection(
+    {
+    host: 'localhost',
+    user: 'root',
+    password: 'BAngkok2017!!',
+    database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
 app.get('/', (req, res) => {
     res.json({
         message: 'Hello World!'
     })
 })
+
+db.query(`SELECT *FROM candidates`, (err, rows) => {
+    console.log(rows);
+})
+
+
 
 app.use((req, res) => {
     res.status(404).end();
